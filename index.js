f74413c643047cd0338770fe3067302b959bf22f
Antonio Robles <antjrobles@gmail.com>
2023-06-10 10:36:32 +0200

    lalalaczxczx

--- index.js
+++ index.js
@@ -8,4 +8,18 @@ canvas.height = window.innerHeight;
 ctx.fillStyle = 'black';  // color
 ctx.fillRect(0, 0, canvas.width, canvas.height)  // x, y, width, height
 
-console.log(ctx);
+
+class Player {
+  constructor({ position, velocity }) {
+    this.position = position;  // { x: 0, y: 0 }
+    this.velocity = velocity
+  }
+};
+
+
+const player = new Player({
+  position: { x: 0, y: 0 },
+  velocity: { x: 0, y: 0 },
+});
+
+console.log(player);
